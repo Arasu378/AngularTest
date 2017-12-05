50626d05405e123b3874efc500f4a85d9fa6108f
KyrosTechnologies <info@kyrostechnologies.com>
2017-12-05 20:04:19 +0530

    some updated test code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import {HeroFormComponent} from './hero/Hero-Form.component';
+import {
+  HeroDetailComponent
+} from './reactiveforms/hero-detail.component';
+import {HeroListComponent} from './reactiveforms/hero-list.component';
+import {HeroService} from './reactiveforms/hero.service';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   declarations: [
     AppComponent,
-    HeroFormComponent
+    HeroFormComponent,
+    HeroDetailComponent,
+    HeroListComponent
   ],
-  providers: [],
+  exports: [ // export for the DemoModule
+    AppComponent,
+    HeroDetailComponent,
+    HeroListComponent
+  ],
+  providers: [ HeroService ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/http/httpcomponent.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+//@Component()
+export class MyComponent implements OnInit {
+
+  results: string[];
+
+  // Inject HttpClient into your component or service.
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    // Make the HTTP request:
+    this.http.get('/api/items').subscribe(data => {
+      // Read the result field from the JSON response.
+      this.results = data['results'];
+    });
+  }
+}
